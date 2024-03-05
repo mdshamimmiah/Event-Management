ab7b77e6f3c9d27c65a410305010a2192645e679
Your Name <www.mdabirahmedsl7162@gmail.com>
2024-03-05 19:50:22 +0600

    first commit

---
+++ src/pages/services/Services.jsx
@@ -0,0 +1,22 @@
+
+
+const Services = () => {
+    return (
+        <div className="mx-auto bg-blue-300 w-[360px] md:w-1/2 lg:w-1/2">
+           <h2 className="text-5xl text-start">Our Services</h2> <hr />
+         <div className="text-start ">
+        <p className="text-2xl"> 1. Wedding Engagement party.</p>
+          <p className="text-2xl">2. Wedding yellow evening.</p>
+         <p className="text-2xl">  3. Wedding Party</p>
+         <p className="text-2xl">4. Wedding Decoration</p>
+         <p className="text-2xl">5. Food Arrangement </p>
+         <p className="text-2xl">6.Photography and Videography </p>
+         <p className="text-2xl">7.  Wedding cultural events </p>
+         <p className="text-2xl">8. wedding gift items</p>
+         <p className="text-2xl">9. Wedding Vendor Payments </p>
+         </div>
+        </div>
+    );
+};
+
+export default Services;
---
+++ src/rules/ServiceRules.jsx
@@ -0,0 +1,20 @@
+
+
+const ServiceRules = () => {
+    return (
+        <div className="bg-orange-300 ">
+           <h2 className="text-5xl text-start ">Service Rules :</h2> 
+           <div className="text-start">
+<h2 className="text-3xl">1. 25% Advance payment</h2><br />
+<h2 className="text-3xl">2. you have to booked minimum 3 days</h2><br />
+<h2 className="text-3xl">3. Not Allow excess use of sound system</h2><br />
+<h2 className="text-3xl">3. Fixed price</h2><br />
+
+
+
+           </div>
+        </div>
+    );
+};
+
+export default ServiceRules;
